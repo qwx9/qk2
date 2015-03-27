762a13333eba9d50e5b186e7047f77d541d34d1a
Konstantinn Bonnet <qu7uux@gmail.com>
2015-03-27 23:58:46 +0100

    disable packing for client.h structs
    packing resulted in bad breakage due to struct members at the end of the definition
    being chopped off.
    just one example of why enabling packing globally (!!!) was a horrible idea to begin with.

--- client/client.h
+++ client/client.h
@@ -21,6 +21,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 //#define	PARANOID			// speed sapping error checking
 
+#pragma pack off
+
 //=============================================================================
 
 typedef struct
@@ -566,3 +568,5 @@ void x86_TimerStop( void );
 void x86_TimerInit( unsigned long smallest, unsigned longest );
 unsigned long *x86_TimerGetHistogram( void );
 #endif
+
+#pragma pack off
--- q_shared.h
+++ q_shared.h
@@ -20,7 +20,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 // q_shared.h -- included first by ALL program modules
 
-#pragma pack on
+#pragma pack on		/* FIXME: amd64 workaround for structs assumed aligned to 4 bytes */
 
 //#define id386
 
